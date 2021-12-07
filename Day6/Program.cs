514313179ad8329519860232f8c7fd2d3143f359
Lars Celie <lars.celie@infosupport.com>
2021-12-07 17:49:50 +0100

    add day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Day6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var inputString = args.Length > 0 ? Input.TestValue : Input.Value;
+
+            var initialDivision = inputString.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => int.Parse(s))
+                .GroupBy(value => value)
+                .ToDictionary(value => value.Key, value => value.LongCount());
+
+            initialDivision[0] = 0;
+            initialDivision[6] = 0;
+            initialDivision[7] = 0;
+            initialDivision[8] = 0;
+
+            var days = 256;
+            var fishTimers = 8;
+
+            for (int i = 1; i <= days; i++)
+            {
+                var duplicateFish = initialDivision[0];
+                for (int j = 0; j < fishTimers; j++)
+                {
+                    initialDivision[j] = initialDivision[j + 1];
+                }
+                
+                initialDivision[6] += duplicateFish;
+                initialDivision[8] = duplicateFish;
+                
+                var dayTotal = initialDivision.Values.Sum();
+                Console.WriteLine($"Fish after {i} days: {dayTotal}");
+            }
+
+            var total = initialDivision.Values.Sum();
+            Console.WriteLine(total);
+        }
+    }
+}
